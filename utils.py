5b48de8ecb6a4d2583cf1141a4ca1209674fa24f
George Michailov <george.michailov.work@outlook.com>
2026-01-18 14:54:25 -0800

    Added managers for UNet Text Encoder and VAE.

---
+++ utils.py
@@ -0,0 +1,123 @@
+from transformers import CLIPTextModel, CLIPTokenizer
+from diffusers.models.autoencoders.autoencoder_kl import AutoencoderKL
+from diffusers.schedulers.scheduling_ddpm import DDPMScheduler 
+import torch
+
+
+class TextEncoderManager:
+    def __init__(self, param_budget_millions, device="cuda", dtype=torch.float16):
+        """
+        Uses CLIP and takes parameter budget into account when selecting which CLIP model to use.
+        Selects the model with the largest number of params below or equal to provided budget.
+        Returns output_dims.
+        """
+        if param_budget_millions < 63:
+            raise ValueError("Smallest Text Encoder has 63M params.")
+        options = {
+            63 : {
+                "model" : "openai/clip-vit-base-patch32",
+                "output_dim" : 512
+            },
+            120 : {
+                "model" : "openai/clip-vit-base-patch14",
+                "output_dim" : 768
+            },
+            300 : {
+                "model" : "laion/CLIP-ViT-H-14-laion2B-s32B-b79K",
+                "output_dim" : 768
+            },
+            700 : {
+                "model" : "laion/CLIP-ViT-bigG-14-laion2B-39B-b160k",
+                "output_dim" : 1280
+            },
+        }
+        selected_params = max([params for params in options.keys() if params <= param_budget_millions])
+        self.tokenizer = CLIPTokenizer.from_pretrained(options[selected_params]["model"])
+        self.encoder = CLIPTextModel.from_pretrained(options[selected_params]["model"], torch_dtype=dtype)
+        self.output_dim = options[selected_params]["output_dim"]
+        self.model_name = options[selected_params]["model"]
+        self.device = device
+        self.encoder.eval()
+        for p in self.encoder.parameters():
+            p.requires_grad = False
+        self.encoder.to(self.device) #type: ignore
+    
+    def encode(self, batch_text):
+        tokens = self.tokenizer(
+            batch_text,
+            padding=True,
+            truncation=True,
+            return_tensors="pt"
+        ).to(self.device)
+        outputs = self.encoder(**tokens)
+        return outputs.last_hidden_state
+
+
+class VAEManager:
+    def __init__(self, param_budget_millions, device="cuda", dtype=torch.float16):
+        if param_budget_millions < 15:
+            raise ValueError("Minimum VAE params is 15M")
+        options = {
+            15 : {
+                "model" : "SG161222/Mobile-VAE-0.4",
+                "downsampling": 16,
+                "channels": 4,
+            },
+            25 : {
+                "model" : "madebyollin/tiny-vae",
+                "downsampling": 8,
+                "channels": 4,
+            },
+            83 : {
+                "model" : "stabilityai/sd-vae-ft-ema",
+                "downsampling": 8,
+                "channels": 4,
+            },
+            90 : {
+                "model" : "stabilityai/sdxl-vae",
+                "downsampling": 8,
+                "channels": 4,
+            },
+        }
+        selected_params = max([params for params in options.keys() if params <= param_budget_millions])
+        self.vae = AutoencoderKL.from_pretrained(options[selected_params]["model"], torch_dtype=dtype)
+        self.downsampling = options[selected_params]["downsampling"]
+        self.channels = options[selected_params]["channels"]
+        self.model_name = options[selected_params]["model"]
+        self.device = device
+        self.scheduler = DDPMScheduler()
+        self.vae = self.vae.to(self.device) #type:ignore
+        self.vae.eval()
+        for p in self.vae.parameters():
+            p.requires_grad = False
+
+    @torch.no_grad()
+    def apply_gaussian_noise_and_encode(self, batch_image_tensors, max_timestep):
+        """
+        Assumes input dims of batch, 3 (channels), H, W (RGB and previously resized).
+        Returns: {
+            noisy_latents : [B, C, H/self.downsampling, W/self.downsampling],
+            noise: [B, C, H/self.downsampling, W/self.downsampling],
+            timesteps: [B]
+        }
+        """
+        batch_image_tensors = batch_image_tensors.to(self.device)
+        latents = self.vae.encode(batch_image_tensors).latent_dist.sample() * self.vae.config.scaling_factor #type: ignore
+        noise = torch.randn_like(latents)
+        timesteps = self._sample_random_timesteps(latents.shape[0], max_timestep=min(self.scheduler.config.num_train_timesteps, max_timestep)) #type: ignore
+        noisy_latents = self.scheduler.add_noise(latents, noise, timesteps) #type: ignore
+        return noisy_latents, noise, timesteps
+
+    def _sample_random_timesteps(self, batch_size, max_timestep):
+        return torch.randint(
+            low=0,
+            high=max_timestep,
+            size=(batch_size,),
+            device=self.device,
+            dtype=torch.long,
+        )
+
+    def decode(self, latents):
+        latents = latents / self.vae.config.scaling_factor #type: ignore
+        images = self.vae.decode(latents).sample # type: ignore
+        return images
